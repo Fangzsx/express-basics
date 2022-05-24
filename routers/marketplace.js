3389a1b2707380ccc18eb65b48913d1d01b5e740
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-24 16:23:24 +0800

    add post

--- routers/marketplace.js
+++ routers/marketplace.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(express.urlencoded({extended : false }))
+
+const items = [
+   { itemName : 'phone', price : 200 },
+   { itemName : 'laptop', price : 400},
+   { itemName : 'tablet', price : 300},
+]
+
+router.post('/', (req, res) => {
+   console.log(req.body);
+   res.status(201).send('post product');
+});
+
 router.get('/', (req, res) => {
    res.send('*showing all listings');
 });
