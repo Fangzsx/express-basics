57b2b1d1ab1d9d05a40ca2afe20938cb8c9ac192
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-24 17:27:49 +0800

    add temp db

--- routers/notifications.js
+++ routers/notifications.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(express.urlencoded({extended : true}))
+router.use(express.json());
+
+//temporary db
+const notificationDB = [
+    { content : "new message1" , timeStamp : new Date(Date.now())},
+    { content : "new message2" , timeStamp : new Date(Date.now())},
+    { content : "new message3" , timeStamp : new Date(Date.now())},
+    { content : "new message4" , timeStamp : new Date(Date.now())},
+]
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(201).send('post notifications');
+})
+
 router.get('/', (req, res) => {
-    res.send(`*showing all notifications`);
+    res.send(notificationDB);
 })
 
 module.exports = router
